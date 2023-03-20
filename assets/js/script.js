bbef2aacf9d212ad6fd10412c08fa450f5f112c7
Cheryl L <cheryl.loeffler@gmail.com>
2023-03-20 14:50:28 -0600

    trying to get things to appear/disappear under appropriate conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,19 +14,27 @@ var timeLeft = 40;
 
 counterEl.textContent = timeLeft;
 
-
-
 var questions = [
     ["what is stuff?",["1", "2", "3", "4"], [0, 0, 0, 1]], 
-    ["what is junk?",["1", "2", "3", "4"], "4"], 
-    ["what is ssadf?",["1", "2", "3", "4"], "4"], 
-    ["what is stsadlasdf",["1", "2", "3", "4"], "4"], 
-    ["what is ;a;lkd?",["1", "2", "3", "4"], "4"], 
+    ["what is junk?",["1", "2", "3", "4"], [0, 0, 0, 1]], 
+    ["what is ssadf?",["1", "2", "3", "4"],[0, 0, 0, 1]], 
+    ["what is stsadlasdf?",["1", "2", "3", "4"], [0, 0, 0, 1]], 
+    ["what is ;a;lkd?",["1", "2", "3", "4"], [0, 0, 0, 1]], 
 ];
+
 const choices = 1;
 const question = 0;
 const answer = 2;
 var questionIndex = 0
+
+function init() {
+    score === 0;
+    timeLeft === 40;
+    //set timer
+    quizBoxEl.setAttribute("data-state", "active");
+    startBoxEl.remove();
+    populateQuestion();
+}
 
 function populateQuestion(){
     var newQuestionEl = document.createElement('h5');
@@ -37,7 +45,7 @@ function populateQuestion(){
         newBtnEl.textContent=questions[questionIndex][choices][j];
         newBtnEl.setAttribute('data-correct', questions[questionIndex][answer][j])
         quizBoxEl.append(newBtnEl);
-}   
+    }   
 }     
     
 
@@ -45,19 +53,18 @@ function populateQuestion(){
 
 //start button
 startButtonEl.addEventListener("click", function(event){
-    //console.log(event);
-    populateQuestion();
+    init();
 });
 
 //quiz answer buttons
 quizBoxEl.addEventListener("click", function(event){
     var element = event.target;
+    console.log(element);
     var isCorrect = document.createElement('h5');
-    if(element.matches('button')) {
-        console.log(element.dataset.correct);
+    if(element.matches("button")) {
         if(element.dataset.correct === "1"){
             isCorrect.textContent = "correct";
-            score + 10;
+            score += 10;
             quizBoxEl.append(isCorrect);
             console.log(score);
         } else {
@@ -65,8 +72,15 @@ quizBoxEl.addEventListener("click", function(event){
             quizBoxEl.append(isCorrect);
         }
     }
-    questionIndex++;
-    populateQuestion();
+    console.log(questionIndex);
+    if (questionIndex === questions.length-1) {
+        questionIndex = 0;
+        window.location.href = './highscores.html';
+    } else {
+        questionIndex++;
+        quizBoxEl.innerHTML = "";
+        populateQuestion();
+    }
 });
 
 
@@ -75,32 +89,3 @@ quizBoxEl.addEventListener("click", function(event){
 
 
 
-
-
-
-
-// var questions = [
-//     {question: "what are things?", answers: ["1", "2", "3", "4"], correct: "4"}, 
-//     {question: "what is stuff?", answers: ["1", "2", "3", "4"], correct: "2"}, 
-//     {question: "what do?", answers: ["1", "2", "3", "4"], correct: "3"}, 
-//     {question: "what is up?", answers: ["1", "2", "3", "4"], correct: "1"}, 
-//     {question: "what is life?", answers: ["1", "2", "3", "4"], correct: "2"}
-// ];
-
-
-    // console.log(questions);
-    // //questionEl.textContent = questions[1].question;
-    // var newQuestionEl = document.createElement('h2');
-    // var newBtnEl = document.createElement('button');
-    // console.log(newBtnEl);
-    // console.log(newQuestionEl);
-
-    // for(var i; i<sizeof(questions); i++){
-    //     newQuestionEl.textContent = questions[i].question;
-    //     console.log(newQuestionEl)
-    //     quizBoxEl.append(newQuestionEl);
-    //     for(var j; j<questions[i].answers.length; j++){
-    //         newBtnEl.textContent = questions[i].answers[j];
-    //         quizBoxEl.append(newBtnEl);
-    //     }
-    // }
